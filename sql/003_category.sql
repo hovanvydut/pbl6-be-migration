845677fa16d753921d8e9a4087ef92d072be2f41
hovanvydut <hovanvydut@gmail.com>
2022-10-15 22:32:13 +0700

    [seed] add seed Category table

---
+++ sql/003_category.sql
@@ -0,0 +1,7 @@
+insert into ExampleDb.category (display_name, slug, created_at)
+values 
+('Phòng ở ghép', 'phong-o-ghep', NOW()),
+('Phòng trong nhà nguyên căn', 'phong-trong-nha-nguyen-can', NOW()),
+('Phòng riêng biệt', 'phong-rieng-biet', NOW()),
+('Nhà nguyên căn', 'nha-nguyen-can', NOW()),
+('Căn hộ', 'can-ho', NOW());
