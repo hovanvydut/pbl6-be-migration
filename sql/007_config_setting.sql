bfea3b6e93204e8dfd133095e76579056ca73683
phuocleoceo <phuoc.truong@emandai.net>
2022-11-08 23:37:22 +0700

    Priority post setting

--- sql/007_config_setting.sql
+++ sql/007_config_setting.sql
@@ -1,4 +1,7 @@
 insert into ExampleDb.config_setting (id, `key`, description, value, created_at) 
 values 
 (1, 'post_price', 'Đơn giá cho một lần đăng bài', 10000, NOW()),
-(2, 'free_post', 'Số lần đăng bài miễn phí cấp cho chủ trọ mới', 5, NOW());
+(2, 'free_post', 'Số lần đăng bài miễn phí cấp cho chủ trọ mới', 5, NOW()),
+(3, 'uptop_price', 'Đơn giá đưa tin lên mục ưu tiên trong 1 ngày', 15000, NOW()),
+(4, 'max_priority_post', 'Số bài đăng ưu tiên tối đa trong 1 khu vực', 5, NOW()),
+(5, 'priority_in_search', 'Số bài đăng ưu tiên hiển thị tối đa trong 1 trang tìm kiếm', 5, NOW());
